8440ae6498182f59d92bc4fea0e81d5211f4ff75
Aramis Sennyey <aramissennyeydd@users.noreply.github.com>
2024-05-10 08:42:20 -0400

    make it not async

--- libraries/rush-lib/src/logic/operations/test/OperationExecutionManager.test.ts
+++ libraries/rush-lib/src/logic/operations/test/OperationExecutionManager.test.ts
@@ -249,7 +249,7 @@ describe(OperationExecutionManager.name, () => {
         );
 
         const result: IExecutionResult = await executionManager.executeAsync();
-        await _printTimeline({ terminal: mockTerminal, result });
+        _printTimeline({ terminal: mockTerminal, result });
         _printOperationStatus(mockTerminal, result);
         const allMessages: string = mockWritable.getAllOutput();
         expect(allMessages).toContain('Build step 1');
